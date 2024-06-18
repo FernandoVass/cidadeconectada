1935e9e2a723278e5bd641e52027bf02819fcfbf
Fernando Vassoler <114179245+FernandoVass@users.noreply.github.com>
2024-06-17 21:04:25 -0300

    Project Spring Boot

---
+++ Cidade Conectada Project/Cidade Conectada/cidadeconectada/src/main/java/com/cidadeconectada/cidadeconectada/CidadeconectadaApplication.java
@@ -0,0 +1,13 @@
+package com.cidadeconectada.cidadeconectada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidadeconectadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidadeconectadaApplication.class, args);
+	}
+
+}
---
+++ Cidade Conectada Project/Cidade Conectada/cidadeconectada/src/test/java/com/cidadeconectada/cidadeconectada/CidadeconectadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cidadeconectada.cidadeconectada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadeconectadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
